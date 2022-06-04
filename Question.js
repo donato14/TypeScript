de360b8fbcda95529579759445049feb8db9982b
Kang Kujun <donato14@naver.com>
2022-06-04 23:22:31 +0900

    ⚡️ Day1 Question

---
+++ Question.js
@@ -0,0 +1,6 @@
+var song = { song: '물', singer: '이적' };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
---
+++ Question.ts
@@ -0,0 +1,13 @@
+// Day1
+type SONG = { song: string, singer: string };
+
+let song: SONG = { song: '물', singer: '이적' };
+
+let project: { [key : string] :string[] | number | boolean } = {
+  member: ['kim', 'park'],
+  days: 30,
+  started: true,
+}
+
+
+
