3012eda650a5127215a0e6a57d3273f275b056fb
Kang Kujun <donato14@naver.com>
2022-06-05 21:02:14 +0900

    ⚡️ unknown, any type

--- Question.js
+++ Question.js
@@ -4,3 +4,15 @@ let project = {
     days: 30,
     started: true,
 };
+// Day2
+let user = 'kim';
+let age = undefined;
+let married = false;
+let 철수 = [user, age, married];
+let 학교 = {
+    score: [100, 97, 84],
+    teacher: 'Phil',
+    friend: 'John'
+};
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher];
--- Question.ts
+++ Question.ts
@@ -11,3 +11,20 @@ let project: { [key : string] :string[] | number | boolean } = {
 
 
 
+// Day2
+let user: string = 'kim';
+let age: unknown = undefined;
+let married: boolean = false;
+let 철수: unknown[] = [user, age, married];
+
+let 학교: {
+  score: (number | boolean)[],
+  teacher: string,
+  friend:string|string[]
+} = {
+  score: [100, 97, 84],
+  teacher: 'Phil',
+  friend : 'John'
+}
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher]
